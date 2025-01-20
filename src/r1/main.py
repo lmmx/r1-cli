40688dd7573a827bdb55edcec11b8f1273d47c8d
Louis Maddox <louismmx@gmail.com>
2025-01-20 21:14:01 +0000

    refactor: put slow imports inside model load function so CLI is fast, add options for all model sizes

--- src/r1/main.py
+++ src/r1/main.py
@@ -1,19 +1,20 @@
 import logging
+from typing import Literal
 
 import argh
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 
 
-class NoEOSTextStreamer(TextStreamer):
-    def on_finalized_text(self, text: str, stream_end: bool = False):
-        if stream_end:
-            eos = self.tokenizer.special_tokens_map["eos_token"]
-            text = text.removesuffix(eos)
-        print(text, flush=True, end="" if not stream_end else None)
+def load_r1(model_size: str):
+    from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 
+    class NoEOSTextStreamer(TextStreamer):
+        def on_finalized_text(self, text: str, stream_end: bool = False):
+            if stream_end:
+                eos = self.tokenizer.special_tokens_map["eos_token"]
+                text = text.removesuffix(eos)
+            print(text, flush=True, end="" if not stream_end else None)
 
-def load_r1():
-    model_name = "unsloth/DeepSeek-R1-Distill-Qwen-7B-bnb-4bit"
+    model_name = f"unsloth/DeepSeek-R1-Distill-Qwen-{model_size}-bnb-4bit"
     logging.getLogger("transformers.utils.quantization_config").setLevel(logging.ERROR)
     model = AutoModelForCausalLM.from_pretrained(model_name, low_cpu_mem_usage=True)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -23,12 +24,13 @@ def load_r1():
 
 def talk_to_r1(
     messages: list[str],
+    model_size: Literal["1.5B", "7B", "14B", "32B"] = "7B",
     max_new_tokens: int | None = None,
     deterministic: bool = False,
     temperature: float | None = None,
     top_p: float | None = None,
 ) -> None:
-    model, tokenizer, streamer = load_r1()
+    model, tokenizer, streamer = load_r1(model_size)
     msg_list = [{"role": "user", "content": msg} for msg in messages]
     prompt_fmt = tokenizer.apply_chat_template(
         msg_list, tokenize=False, add_generation_prompt=True
