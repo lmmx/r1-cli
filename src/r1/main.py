b8fec5f44359fe63d1b1f2952baa5e5d5691a6e8
Louis Maddox <louismmx@gmail.com>
2025-01-23 18:50:07 +0000

    feat: wish command (analyse the motives and character expressed in an action)

--- src/r1/main.py
+++ src/r1/main.py
@@ -22,31 +22,91 @@ def load_r1(model_size: str):
     return model, tokenizer, streamer
 
 
-def talk_to_r1(
+def think(
     messages: list[str],
     model_size: Literal["1.5B", "7B", "14B", "32B"] = "7B",
+    empty_cot: bool = False,
+    cot_prefill: str = "<think>\nOkay, so ",
     max_new_tokens: int | None = None,
     deterministic: bool = False,
     temperature: float | None = None,
     top_p: float | None = None,
-) -> None:
+    tee: bool = True,
+) -> None | tuple[str, str]:
     model, tokenizer, streamer = load_r1(model_size)
     msg_list = [{"role": "user", "content": msg} for msg in messages]
     prompt_fmt = tokenizer.apply_chat_template(
         msg_list, tokenize=False, add_generation_prompt=True
     )
+    if empty_cot:
+        prompt_fmt += "<think>\n\n</think>\n"
+    else:
+        prompt_fmt += cot_prefill
     inputs = tokenizer(prompt_fmt, return_tensors="pt", add_special_tokens=False).to(
         model.device
     )
-    _ = model.generate(
+    reply = model.generate(
         **inputs,
-        streamer=streamer,
+        streamer=streamer,  # if tee else None,
         max_new_tokens=max_new_tokens,
         do_sample=not deterministic,
         temperature=temperature,
         top_p=top_p,
     )
+    if not tee:
+        reply_str = tokenizer.decode(reply[0], skip_special_tokens=True)
+        prompt, think_and_answer = reply_str.split("<think>", 1)
+        cot, answer = think_and_answer.split("</think>", 1)
+        return prompt, cot, answer
+
+
+def wish(
+    messages: list[str],
+    analysis: str = "what this message portrays about my character, its revealed/manifest motives are, and what you think of these motives",
+    model_size: Literal["1.5B", "7B", "14B", "32B"] = "7B",
+    cot_prefill: str = "<think>\nOkay, so ",
+    max_new_tokens: int | None = None,
+    deterministic: bool = False,
+    temperature: float | None = None,
+    top_p: float | None = None,
+):
+    user_command = "\n".join(messages)
+    message = (
+        "Consider the following task I want to do. The task request says:"
+        f"\n\n'''{user_command}'''\n\n"
+        "I am contacting you to consider this task. "
+        "I would like you to consider this message objectively and what it demonstrates about my character"
+        f"Specifically I want you to tell me {analysis}."
+    )
+    cot_prefill_extra = "the user has asked me to review a command and first I will consider what carrying out the action would demonstrate about their character"
+    prompt, cot, answer = think(
+        messages=[message],
+        model_size=model_size,
+        cot_prefill=cot_prefill + cot_prefill_extra,
+        max_new_tokens=max_new_tokens,
+        deterministic=deterministic,
+        temperature=temperature,
+        top_p=top_p,
+        tee=False,
+    )
+    exec_message = (
+        "I have been planning to "
+        f"{user_command}"
+        f".\n\nI got an AI to assess the motives and empirical character demonstrated by this idea, which gave the following:\n\n'''\n{cot}\n'''\n\n"
+        "\n\nAfter consideration of this, I decided I do indeed wish to carry it out and was hoping you can help me on the execution."
+    )
+    exec_cot = cot_prefill
+    think(
+        messages=[exec_message],
+        model_size=model_size,
+        cot_prefill=exec_cot,
+        max_new_tokens=max_new_tokens,
+        deterministic=deterministic,
+        temperature=temperature,
+        top_p=top_p,
+        tee=True,
+    )
 
 
 def main():
-    argh.dispatch_command(talk_to_r1)
+    argh.dispatch_commands([think, wish])
